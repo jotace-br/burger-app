e748814f0d5f69d5f52522212e7fc48a8428af00
Júlio César <jc10ferreira@gmail.com>
2023-12-09 00:49:35 -0300

    feat: :sparkles: Adds a theme provider to make theme-related changes easier

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { AppRouter } from './app-router.tsx';
+import { WebSettingsProvider } from './theme-provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter />
+    <WebSettingsProvider>
+      <AppRouter />
+    </WebSettingsProvider>
   </React.StrictMode>
 );
---
+++ src/theme-provider.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { fetchRestaurantDetails } from './api/api';
+import { WebSettings } from './types/types';
+
+interface WebSettingsProviderProps {
+  children: React.ReactNode;
+}
+
+const defaultWebSettingsContext = {
+  id: 5854,
+  venueId: 7602,
+  bannerImage:
+    'https://preodemo.gumlet.io/usr/venue/7602/web/646fbf3abf9d0.png',
+  backgroundColour: '#ffffff',
+  primaryColour: '#4f372f',
+  primaryColourHover: '#4f372f',
+  navBackgroundColour: '#4f372f',
+};
+
+const WebSettingsContext = createContext<WebSettings | undefined>(
+  defaultWebSettingsContext
+);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useWebSettings = () => useContext(WebSettingsContext);
+
+export const WebSettingsProvider = ({ children }: WebSettingsProviderProps) => {
+  const [webSettings, setWebSettings] = useState<WebSettings>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchRestaurantDetails();
+        const { webSettings } = response;
+        setWebSettings(webSettings);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+        setWebSettings(defaultWebSettingsContext);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <WebSettingsContext.Provider value={webSettings}>
+      {children}
+    </WebSettingsContext.Provider>
+  );
+};
