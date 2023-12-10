d0e025af02688e3622b195a518531313100ac466
Júlio César <jc10ferreira@gmail.com>
2023-12-10 11:03:08 -0300

    feat: :sparkles: Creates a new button for modal

---
+++ src/components/product-modal/add-to-order.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+// TODO: Transform this into a generic button for future use
+export const BtnAddToOrder = styled.button<{ $bgColor?: string }>`
+  color: #fff;
+  border: none;
+  cursor: pointer;
+
+  height: 48px;
+  padding: 4px 24px;
+
+  border-radius: 40px;
+  background: ${(p) => p.$bgColor || '#4f372f'};
+
+  font-size: 16px;
+  font-weight: 500;
+
+  transition: 0.3s background-color ease-in-out;
+
+  &:hover {
+    background-color: #3f2c25;
+  }
+
+  &:disabled {
+    background-color: #dadada;
+  }
+`;
