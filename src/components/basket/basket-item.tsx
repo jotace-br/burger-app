4d6ba2b74cfd1aa1249f724986512858e330fac5
Júlio César <jc10ferreira@gmail.com>
2023-12-12 05:52:36 -0300

    feat: :sparkles: Added the option to open the product edit modal from checkout

--- src/components/basket/basket-item.tsx
+++ src/components/basket/basket-item.tsx
@@ -7,15 +7,40 @@ import { PlusIcon } from '@/assets/icons/plus-icon';
 import './basket-item.css';
 import { useCheckout } from '@/contexts/checkout-content';
 import { TrashcanIcon } from '@/assets/icons/trashcan-icon';
+import { useState } from 'react';
+import { ProductEditModal } from '../product-edit-modal';
+import { IMenu, IMenuItem } from '@/types/menu';
+import { useDataFetcher } from '@/hooks/use-data-fetcher';
+import { fetchMenuDetails } from '@/api/api';
 
 export interface BasketItemProps {
   basketItem: CheckoutObject;
 }
 
 export const BasketItem = ({ basketItem }: BasketItemProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState<IMenuItem>();
+
+  const { data } = useDataFetcher<IMenu>(fetchMenuDetails);
   const { editCheckoutQuantityItem, removeFromCheckout } = useCheckout();
 
   const webSettings = useWebSettings();
+
+  const handleEditModal = async (event: React.MouseEvent) => {
+    const target = event.target as Element;
+    const isClickInQuantityWrapper = target.closest('.basket-quantity-wrapper');
+
+    if (isClickInQuantityWrapper) {
+      return;
+    }
+
+    const selectedItem = await data?.sections
+      ?.flatMap((section) => section.items)
+      .find((item) => item.id === basketItem.refIdProduct);
+
+    setModalContent({ ...selectedItem, ...basketItem } as IMenuItem);
+    setIsModalOpen(!isModalOpen);
+  };
 
   const handleIncrement = () => {
     if (basketItem.refIdProduct) {
@@ -69,26 +94,34 @@ export const BasketItem = ({ basketItem }: BasketItemProps) => {
   };
 
   return (
-    <div className='basket-item'>
-      <div className='basket-label-wrapper'>
-        <p>{basketItem.name}</p>
-        <p>{numberToBRL((basketItem.price || 0) * basketItem.quantity)}</p>
-      </div>
-      {basketItem.modifierProps && (
-        <div className='basket-label-selected-modifier'>
-          <p>{basketItem.modifierProps.name}</p>
+    <>
+      <div className='basket-item' onClick={handleEditModal}>
+        <div className='basket-label-wrapper'>
+          <p>{basketItem.name}</p>
+          <p>{numberToBRL((basketItem.price || 0) * basketItem.quantity)}</p>
         </div>
-      )}
+        {basketItem.modifierProps && (
+          <div className='basket-label-selected-modifier'>
+            <p>{basketItem.modifierProps.name}</p>
+          </div>
+        )}
 
-      <div className='basket-quantity-wrapper'>
-        <div className='basket-quantity-controls'>
-          {renderBasketDecrementBtn()}
-          <span className='basket-quantity-value'>{basketItem.quantity}</span>
-          <BtnQuantity $height='20px' $width='20px' onClick={handleIncrement}>
-            <PlusIcon />
-          </BtnQuantity>
+        <div className='basket-quantity-wrapper'>
+          <div className='basket-quantity-controls'>
+            {renderBasketDecrementBtn()}
+            <span className='basket-quantity-value'>{basketItem.quantity}</span>
+            <BtnQuantity $height='20px' $width='20px' onClick={handleIncrement}>
+              <PlusIcon />
+            </BtnQuantity>
+          </div>
         </div>
       </div>
-    </div>
+
+      <ProductEditModal
+        isProductModalOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        selectedProductToEdit={modalContent}
+      />
+    </>
   );
 };
