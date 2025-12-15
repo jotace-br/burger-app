1e640a88ad46dc90c295e33da232d33d09326ea1
Júlio César <jc10ferreira@gmail.com>
2025-12-15 17:26:10 -0300

    refactor(api): replace API calls with mock data

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,22 +1,25 @@
-import { IMenu } from '@/types/menu';
-import { IRestaurant } from '@/types/types';
+import { mockMenuDetails, mockRestaurantDetails } from '@/__mocks__/mocks';
 
-const API_BASE_URL = 'https://run.mocky.io/v3/';
+// DEPRECATED: Use API service from src/mocks.ts instead
+// const API_BASE_URL = 'https://run.mocky.io/v3/';
 
 export const fetchRestaurantDetails = async () => {
-  const restaurantDetailsReference = '6ac3efc2-2d15-47bd-9cb7-1931a3774ee3';
-  const response: IRestaurant = await fetch(
-    `${API_BASE_URL}${restaurantDetailsReference}`
-  ).then((res) => res.json());
+	// const restaurantDetailsReference = '6ac3efc2-2d15-47bd-9cb7-1931a3774ee3';
 
-  return response;
+	// const response: IRestaurant = await fetch(
+	//   `${API_BASE_URL}${restaurantDetailsReference}`
+	// ).then((res) => res.json());
+
+	// return response;
+	return mockRestaurantDetails;
 };
 
 export const fetchMenuDetails = async () => {
-  const menuDetailsReference = 'c74801f2-0392-4037-a73f-84f01c3939fb';
-  const response: IMenu = await fetch(
-    `${API_BASE_URL}${menuDetailsReference}`
-  ).then((res) => res.json());
+	// const menuDetailsReference = 'c74801f2-0392-4037-a73f-84f01c3939fb';
+	// const response: IMenu = await fetch(
+	//   `${API_BASE_URL}${menuDetailsReference}`
+	// ).then((res) => res.json());
 
-  return response;
+	// return response;
+	return mockMenuDetails;
 };
